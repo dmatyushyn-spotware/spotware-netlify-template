9d4eddd1193296da879b9bc8156bd657e086392a
dmatyushyn-spotware <127385708+dmatyushyn-spotware@users.noreply.github.com>
2025-06-23 18:30:14 +0300

    Update and rename spotwareClient.jsx to spotwareClient.js

---
+++ src/spotwareClient.js
@@ -0,0 +1,36 @@
+import { createClientAdapter } from '@spotware-web-team/sdk-external-api'
+import { registerEvent, handleConfirmEvent } from '@spotware-web-team/sdk'
+import { take, tap, catchError } from 'rxjs'
+import {createLogger} from '@veksa/logger';
+
+let client = null
+
+export const connect = async (setStatus = () => {}) => {
+  const logger = createLogger(true)
+  client = createClientAdapter({ logger })
+
+  try {
+    handleConfirmEvent(client, {})
+      .pipe(take(1))
+      .subscribe()
+
+    registerEvent(client)
+      .pipe(
+        take(1),
+        tap(() => {
+          handleConfirmEvent(client, {}).pipe(take(1)).subscribe()
+          console.log('Connected')
+          setStatus('Connected')
+        }),
+        catchError((error) => {
+          console.error('Connection failed:', error)
+          setStatus('Connection failed')
+          return []
+        })
+      )
+      .subscribe()
+  } catch (err) {
+    console.error('Connection error:', err)
+    setStatus('Connection error')
+  }
+}
