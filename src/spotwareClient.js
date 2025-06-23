ea7bffcfd681234261bfccf6b316f6ad3a612900
dmatyushyn-spotware <127385708+dmatyushyn-spotware@users.noreply.github.com>
2025-06-23 17:06:31 +0300

    Update spotwareClient.js

--- src/spotwareClient.js
+++ src/spotwareClient.js
@@ -1,3 +1,16 @@
-import {createClientAdapter} from '@spotware-web-team/sdk-external-api';
+import { createClientAdapter } from '@spotware-web-team/sdk-external-api';
+import { confirmEvent, registerEvent } from '@spotware-web-team/sdk';
+import { mergeMap, tap, catchError } from 'rxjs';
 
 const clientApi = createClientAdapter();
+
+registerEvent(clientApi).pipe(
+  mergeMap(() => confirmEvent(clientApi)),
+  tap(() => {
+    console.log('Connected');
+  }),
+  catchError((err) => {
+    console.error('Connection failed:', err);
+    return []; // or EMPTY
+  })
+).subscribe();
