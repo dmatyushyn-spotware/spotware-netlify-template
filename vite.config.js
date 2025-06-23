a86013b574a14f42812cbc3082167647768ef114
dmatyushyn-spotware <127385708+dmatyushyn-spotware@users.noreply.github.com>
2025-06-23 15:44:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@spotware/external-api']
+      external: ['@spotware-web-team/external-api']
     }
   }
 });
