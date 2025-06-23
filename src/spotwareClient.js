4a81679e595f428550179bdbfabe6f53773de0ec
dmatyushyn-spotware <127385708+dmatyushyn-spotware@users.noreply.github.com>
2025-06-23 18:06:41 +0300

    Update spotwareClient.js

--- src/spotwareClient.js
+++ src/spotwareClient.js
@@ -1,11 +1,15 @@
 import { createClientAdapter } from '@spotware-web-team/sdk-external-api'
-import { registerEvent, handleConfirmEvent } from '@spotware-web-team/sdk'
+import {
+  registerEvent,
+  handleConfirmEvent,
+  getAccountInformation,
+} from '@spotware-web-team/sdk'
 import { take, tap, catchError } from 'rxjs'
-import {createLogger} from '@veksa/logger';
+import { createLogger } from '@veksa/logger'
 
 let client = null
 
-export const connect = async (setStatus = () => {}) => {
+export const connect = async (setStatus = () => {}, setAccountInfo = () => {}) => {
   const logger = createLogger(true)
   client = createClientAdapter({ logger })
 
@@ -19,18 +23,33 @@ export const connect = async (setStatus = () => {}) => {
         take(1),
         tap(() => {
           handleConfirmEvent(client, {}).pipe(take(1)).subscribe()
-          console.log('Connected')
+
+          getAccountInformation(client)
+            .pipe(
+              take(1),
+              tap((accountInfo) => {
+                logger.log('Account info:', accountInfo)
+                setAccountInfo(accountInfo) // если передали сеттер, сохранить
+              }),
+              catchError((err) => {
+                logger.error('Failed to get account info:', err)
+                return []
+              })
+            )
+            .subscribe()
+
+          logger.log('Connected')
           setStatus('Connected')
         }),
         catchError((error) => {
-          console.error('Connection failed:', error)
+          logger.error('Connection failed:', error)
           setStatus('Connection failed')
           return []
         })
       )
       .subscribe()
   } catch (err) {
-    console.error('Connection error:', err)
+    logger.error('Connection error:', err)
     setStatus('Connection error')
   }
 }
